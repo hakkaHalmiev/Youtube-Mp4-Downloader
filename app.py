975a2871ff7e613086f14469f0eeb18398d5b16f
Hakka Halmiev <hakkahalmiev@gmail.com>
2025-10-03 16:59:06 +0300

    Adding some virtual attributes.

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-url = input("Enter YouTube URL: ")
-yt = YouTube (url, on_progress_callback=on_progress)
-print(yt.title)
+# Add New Libraries to operate virtual program
 
-ys = yt.streams.get_highest_resolution()
-print("Wait.......")
-ys.download()
-print('Completed')
+import os
+import tkinter as tk
+from tkinter import filedialog,messagebox
+
+
+#url = input("Enter YouTube URL: ")
+#yt = YouTube (url, on_progress_callback=on_progress)
+#print(yt.title)
+
+#ys = yt.streams.get_highest_resolution()
+#print("Wait.......")
+#ys.download()
+#('Completed')
 
 # Add virtual settings
+
+## Create a window
+
+root = tk.Tk()
+root.title("Youtube MP4 Downloader")
+root.geometry("600x200")
+
+label = tk.Label(root, text="MP4 Downloader", font=("Arial", 20))
+label.pack()
+
+# Adding function to download Video
+def download():
+    url = input("Enter URL: ")
+    yt = YouTube(url, on_progress_callback=on_progress)
+
+    ys = yt.streams.get_highest_resolution()
+    ys.download()
+
+### Creating Button
+button = tk.Button(root, text="Download", command=download)
+button.pack()
+
+
+
+
+
+
+root.mainloop()
